9e1d454ac40cccae92a61de1e418e57123fea344
Rafael Nowrotek <rexblack@users.noreply.github.com>
2022-03-10 23:46:35 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const scssf = require('cssf/int/sass');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: [
+    path.resolve(__dirname, 'src/bootstrap.scss')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sassOptions: {
+              includePaths: [
+                path.resolve(__dirname, 'node_modules'),
+              ],
+              functions: scssf
+            }
+          }
+        }
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ]
+};
